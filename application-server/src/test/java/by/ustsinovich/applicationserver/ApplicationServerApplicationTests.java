46061558a3d4a79ae362990f0b7b5cc24bf7a902
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-03-10 11:51:29 +0300

    init

---
+++ application-server/src/test/java/by/ustsinovich/applicationserver/ApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.applicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
